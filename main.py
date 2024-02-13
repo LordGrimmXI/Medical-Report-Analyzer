c5d6f63cd6b32de6ff376279303b07e33eae91de
Sushant Shubham <55201951+LordGrimmXI@users.noreply.github.com>
2024-02-13 14:47:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import boto3
+import json
+import pandas as pd
+
+s3 = boto3.client('comprehendmedical', region_name = 'ap-southeast-2', aws_access_key_id = 'AKIAWRU3YRNO3ZA7OE2E', aws_secret_access_key = 'OD2teiKAxjk5Q/y3vhu4XJ52UqS6wNL5UJTJ+dEM')
+
+def get_medical_report():
+    while True:
+        text = input("Enter your Medical Report: ")
+        if text.strip():
+            return text
+        else:
+            print("Please provide a valid input.")
+
+medical_report = get_medical_report()
+
+print('Calling infer_icd10_cm')
+try:
+    infer_icd10_cm_response = s3.infer_icd10_cm(Text=medical_report)
+    # Write the response to a JSON file
+    with open('infer_icd10_cm_response_medical_data.json', 'w') as outfile:
+        json.dump(infer_icd10_cm_response, outfile, indent=4)
+    print("Response written to 'infer_icd10_cm_response_medical_data.json'")
+    print(json.dumps(infer_icd10_cm_response, indent=4))
+except Exception as e:
+    print("Error:", e)
+
+with open('infer_icd10_cm_response_medical_data.json', 'r') as file:
+    data = json.load(file)
+
+import pandas as pd
+
+with open('infer_icd10_cm_response_medical_data.json', 'r') as file:
+    data = json.load(file)
+
+df = pd.json_normalize(data['Entities'])
+
+df.to_csv('medical_data.csv', index=False)
+
+print("CSV file saved successfully.")
+
+print("\n", df.loc[:, ["Text", "Category", "Score"]])
